8c92ee05f7c849ada6fa64ba022866c8be1a74f4
Pradeep <Pradeep.akarsh92@gmail.com>
2024-04-27 22:18:57 +0530

    Second_Demo file to move

--- src/main/java/basics/common/Selenium_Base_Example.java
+++ src/main/java/basics/common/Selenium_Base_Example.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class SeleniumExample {
+public class Selenium_Base_Example {
 
     public static void main(String[] args) {
 
